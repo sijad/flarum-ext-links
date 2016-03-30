98468ca11b6c2faf6a5ce4fb30985a62225d0de9
Sajjad Hasehmian <wolaws@gmail.com>
2016-03-30 13:27:24 +0430

    Update Migration

--- migrations/2016_02_13_000000_create_links_table.php
+++ migrations/2016_02_13_000000_create_links_table.php
@@ -4,27 +4,18 @@
  * (c) Sajjad Hashemian <wolaws@gmail.com>
  */
 
-namespace Sijad\Links\Migration;
-
-use Flarum\Database\AbstractMigration;
+use Flarum\Database\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateLinksTable extends AbstractMigration
-{
-    public function up()
-    {
-        $this->schema->create('links', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title', 50);
-            $table->string('type', 30);
-            $table->string('url', 255);
-            $table->integer('ref_id')->unsigned()->nullable();
-            $table->integer('position')->nullable();
-        });
+return Migration::createTable(
+    'links',
+    function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('title', 50);
+        $table->string('type', 30);
+        $table->string('url', 255);
+        $table->integer('ref_id')->unsigned()->nullable();
+        $table->integer('position')->nullable();
     }
+);
 
-    public function down()
-    {
-        $this->schema->drop('links');
-    }
-}
