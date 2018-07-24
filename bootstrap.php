8f399795ec6874bbbcfe7de3180a2bc75092ccfa
Sajjad Hashemian <wolaws@gmail.com>
2018-07-24 14:33:30 +0430

    use Extend\Routes for API

--- bootstrap.php
+++ bootstrap.php
@@ -6,17 +6,26 @@
 
 use Flarum\Extend;
 use Sijad\Links\Listener;
+use Sijad\Links\Api\Controller;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return [
     new Extend\Locales(__DIR__.'/locale'),
+
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js'),
+
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js')
         ->css(__DIR__.'/less/admin.less'),
+
+    (new Extend\Routes('api'))
+        ->post('/links', 'links.create', Controller\CreateLinkController::class)
+        ->post('/links/order', 'links.order', Controller\OrderLinksController::class)
+        ->patch('/links/{id}', 'links.update', Controller\UpdateLinkController::class)
+        ->delete('/links/{id}', 'links.delete', Controller\DeleteLinkController::class),
+
     function (Dispatcher $events) {
-        $events->subscribe(Listener\AddLinksApi::class);
         $events->subscribe(Listener\AddLinksRelationship::class);
     }
 ];
