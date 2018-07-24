11e7d20905efaadbc1ed628e1aa153bc93d73173
Sajjad Hashemian <wolaws@gmail.com>
2018-07-24 14:21:44 +0430

    use new extender

--- bootstrap.php
+++ bootstrap.php
@@ -4,11 +4,18 @@
  * (c) Sajjad Hashemian <wolaws@gmail.com>
  */
 
+use Flarum\Extend;
 use Sijad\Links\Listener;
 use Illuminate\Contracts\Events\Dispatcher;
 
-return function (Dispatcher $events) {
-    $events->subscribe(Listener\AddClientAssets::class);
-    $events->subscribe(Listener\AddLinksApi::class);
-    $events->subscribe(Listener\AddLinksRelationship::class);
-};
+return [
+    (new Extend\Frontend('forum'))
+        ->js(__DIR__.'/js/dist/forum.js'),
+    (new Extend\Frontend('admin'))
+        ->js(__DIR__.'/js/dist/admin.js')
+        ->css(__DIR__.'/less/admin.less'),
+    function (Dispatcher $events) {
+        $events->subscribe(Listener\AddLinksApi::class);
+        $events->subscribe(Listener\AddLinksRelationship::class);
+    }
+];
